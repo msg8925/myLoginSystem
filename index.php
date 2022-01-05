2929663f146a7daa29a4190f39018af09b2f605b
UnknownAuthor <msg.8925@gmail.com>
2022-01-05 17:15:06 +0700

    Created a login form.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="email" placeholder="E-mail...">
+      <input type="text" name="password" placeholder="Password...">
+      <button type="submit" name="login">Login</button>
+    </form>
+  </body>
+</html>
